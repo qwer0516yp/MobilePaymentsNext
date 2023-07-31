59b8826cc210055c9eb508b224e8714fc7643839
pyua459 <qwer0516yp@gmail.com>
2023-07-31 11:47:33 +1000

    Add secrets razor page and persist kid and rsa key plaintext values with injected UserState

---
+++ BlazorWebAssembly/SecretVaultState.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+
+namespace BlazorWebAssembly;
+
+public class SecretVaultState
+{
+    public string? JwePublicCert { get; set; }
+	public string? JwsPrivatePem { get; set; }
+    public string? JweHeaderKid { get; set; }
+    public string? JwsHeaderKid { get; set; }
+	public RSA? JweEncryptionPublicKey { get; set; }
+	public RSA? JwsSigningPrivateKey { get; set; }
+
+	public bool LoadRSAKeys()
+	{
+		throw new NotImplementedException("To load rsa keys from .Cer or Pem");
+	}
+}
+
