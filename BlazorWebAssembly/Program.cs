f5b6e19e4a7dd9d63d9e9d75b75174b432b36b26
pyua459 <qwer0516yp@gmail.com>
2023-07-31 14:33:12 +1000

    Add toaster implementation on Secrets setting page

--- BlazorWebAssembly/Program.cs
+++ BlazorWebAssembly/Program.cs
@@ -1,4 +1,5 @@
 using BlazorWebAssembly;
+using Blazr.Demo.Toaster;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<SecretVaultState>();
+builder.Services.AddScoped<ToasterService>();
 
 await builder.Build().RunAsync();
---
+++ BlazorWebAssembly/Toaster/Toast.cs
@@ -0,0 +1,33 @@
+namespace Blazr.Demo.Toaster;
+
+public enum MessageColour
+{
+    Primary, Secondary, Dark, Light, Success, Danger, Warning, Info
+}
+
+public record Toast
+{
+    public Guid Id = Guid.NewGuid();
+    public string Title { get; init; } = string.Empty;
+    public string Message { get; init; } = string.Empty;
+    public MessageColour MessageColour { get; init; } = MessageColour.Primary;
+    public readonly DateTimeOffset Posted = DateTimeOffset.Now;
+    public DateTimeOffset TimeToBurn { get; init; } = DateTimeOffset.Now.AddSeconds(10);
+    public bool IsBurnt => TimeToBurn < DateTimeOffset.Now;
+    private TimeSpan elapsedTime => Posted - DateTimeOffset.Now;
+
+    public string ElapsedTimeText =>
+        elapsedTime.Seconds > 60
+        ? $"posted {-elapsedTime.Minutes} mins ago"
+        : $"posted {-elapsedTime.Seconds} secs ago";
+
+
+    public static Toast NewToast(string title, string message, MessageColour messageColour, int secsToLive)
+        => new Toast
+        {
+            Title = title,
+            Message = message,
+            MessageColour = messageColour,
+            TimeToBurn = DateTimeOffset.Now.AddSeconds(secsToLive)
+        };
+}
